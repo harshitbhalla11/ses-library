27f922869c81d0cef15ffa88686d1bd9cb826a9c
harshit.bhalla.002 <harshit.bhalla.002@gmail.com>
2024-03-18 22:29:58 +0000

    update library

--- ses_mail/send_ses.py
+++ ses_mail/send_ses.py
@@ -1,18 +1,20 @@
 import boto3
 
-class SESMailer:
+class SESEmailService:
+
     # default region set to 'eu-north-1 (Stockholm)'
-    def __init__(self, access_key, secret_key, region_name='eu-north-1'):
+    def __init__(self, access_key, secret_key, region='eu-north-1'):
         self.client = boto3.client('ses', 
                                    aws_access_key_id=access_key,
                                    aws_secret_access_key=secret_key,
-                                   region_name=region_name)
+                                   region_name=region)
     
-    def send_email(self, sender_email, recipient_email, subject, body):
+    # send email to the recipient using aws SES.
+    def send_email_ses(self, sender_mail, recipient_mail, subject, body):
         try:
             response = self.client.send_email(
-                Source=sender_email,
-                Destination={'ToAddresses': [recipient_email]},
+                Source=sender_mail,
+                Destination={'ToAddresses': [recipient_mail]},
                 Message={
                     'Subject': {'Data': subject},
                     'Body': {'Text': {'Data': body}}
@@ -21,13 +23,12 @@ class SESMailer:
             print("Email sent! Message ID:", response['MessageId'])
             return True
         except Exception as e:
-            print("Email sending failed:", str(e))
+            print("Sending Email failed")
             return False
         
 
     # user needs to verify the email address before sending/ recieving email so this will verify the user.
-        
-    def verify_email_identity(self, identify_email_address):
+    def Identify_user_email(self, identify_email_address):
         try:
             self.client.verify_email_identity(
                 EmailAddress = identify_email_address
@@ -35,7 +36,7 @@ class SESMailer:
             print(f"Verification email sent to {identify_email_address}.")
             return True
         except Exception as e:
-            print(f"Failed to send verification email to {identify_email_address}: {str(e)}")
+            print(f"Failed to verify email address")
             return False
 
 
